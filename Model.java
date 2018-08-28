4979ab4f4a54f1ea8b174a55422aebbe9886ec16
sudhravi <42777869+sudhravi@users.noreply.github.com>
2018-08-28 21:59:26 +0530

    initial commit

---
+++ Model.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Model {
+	public static void main(String args[]) {
+		System.out.println("Enter number:");
+		Scanner in= new Scanner(System.in);
+		int a=in.nextInt();
+		System.out.println("Hello "+a);
+	} 
+}
